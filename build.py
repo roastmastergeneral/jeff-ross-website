98a3ab2bef59e9ff996ff86e15d4e749ae4168e5
Guilherme Mattar Bastos <guilhermembastos@gmail.com>
2015-08-07 01:21:27 -0300

    Split database in two, developer and production.

--- build.py
+++ build.py
@@ -1,27 +1,32 @@
+# -*- coding: utf-8 -*-
+
 from os import path, getcwd, makedirs
 from shutil import rmtree
 from staticjinja import make_site
 
-
 # We define constants for the deployment.
-outputpath = path.join(getcwd(), 'site')
+_SEARCHPATH = path.join(getcwd(), 'templates')
+_OUTPUTPATH = path.join(getcwd(), 'site')
 
 
-# Clean the output folder.
-if path.exists(outputpath):
-    rmtree(outputpath)
+def cleanup():
+    # Clean the output folder.
+    if path.exists(_OUTPUTPATH):
+        rmtree(_OUTPUTPATH)
+
+    makedirs(_OUTPUTPATH)
 
 
-makedirs(outputpath)
+if __name__ == '__main__':
+    site = make_site(
+        filters={},
+        outpath=_OUTPUTPATH,
+        contexts=[(r'.*.html',)],
+        searchpath=_SEARCHPATH,
+        staticpaths=['static']
+    )
 
+    cleanup()
 
-site = make_site(
-    filters={},
-    outpath=outputpath,
-    contexts=[(r'.*.html',)],
-    staticpaths=['static']
-)
-
-
-# site.render()
-site.render(use_reloader=True)
+    # site.render()
+    site.render(use_reloader=True)

d37fccd593fc67a76282241db161a539e54b132b
Rafael Bicalho <rbmbika@gmail.com>
2018-03-06 21:26:17 -0300

    Update Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,21 @@
-var del             = require('del'),
-    gulp            = require('gulp'),
-    sass            = require('gulp-sass'),
-    shell           = require('gulp-shell'),
-    uglify          = require('gulp-uglify'),
-    ghPages         = require('gulp-gh-pages'),
-    imagemin        = require('gulp-imagemin'),
-    sourcemaps      = require('gulp-sourcemaps'),
-    browserSync     = require('browser-sync'),
-    runSequence     = require('run-sequence').use(gulp),
-    nunjucksRender  = require('gulp-nunjucks-render');
+var del = require('del');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var shell = require('gulp-shell');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence').use(gulp);
+var nunjucksRender = require('gulp-nunjucks-render');
 
-// Clean Dist
-gulp.task('clean', function () {
+// Clean "public" folder
+gulp.task('clean', function() {
   return del(['public']);
 });
 
 // Browser Sync
-gulp.task('browserSync', function () {
+gulp.task('browserSync', function() {
   browserSync({
     server: {
       baseDir: 'public'
@@ -25,8 +24,7 @@ gulp.task('browserSync', function () {
 });
 
 // Sass
-gulp.task('sass', function () {
-  // Gets all files ending with .scss in source/sass
+gulp.task('sass', function() {
   return gulp.src('source/sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'compressed' })).on('error', sass.logError)
@@ -35,60 +33,53 @@ gulp.task('sass', function () {
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('vendor', function () {
+gulp.task('vendor', function() {
   return gulp.src('source/static/vendor/**/*')
     .pipe(gulp.dest('public/vendor'));
 });
 
-gulp.task('image', function () {
+gulp.task('image', function() {
   return gulp.src('source/static/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('public/img'));
 });
 
-// See why .nojekyll: http://stackoverflow.com/q/40453806/4142567
-gulp.task('custom-files', function () {
+gulp.task('custom-files', function() {
   return gulp.src(['source/templates/CNAME', 'source/templates/.nojekyll'])
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('js', function () {
+gulp.task('js', function() {
   return gulp.src('source/static/**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('public'));
 });
 
 // Nunjucks
-gulp.task('nunjucks', function () {
+gulp.task('nunjucks', function() {
   nunjucksRender.nunjucks.configure(['source/templates/']);
 
-  // Gets .html and .nunjucks files in pages
   return gulp.src('source/templates/**/[^_]*.html')
-    // Renders template with nunjucks
     .pipe(nunjucksRender({ path: 'source/templates' }))
-    // output files in app folder
     .pipe(gulp.dest('public'))
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('push-gh-master', shell.task(['git push origin master']));
+gulp.task('git-push', shell.task([
+  'git push origin master',
+  'git subtree push --prefix public origin gh-pages',
+]))
 
-gulp.task('push-gh-pages', function () {
-  return gulp.src('public/**/*', { dot: true })
-    .pipe(ghPages({ force: true }));
-});
-
-gulp.task('deploy', function (callback) {
+// IMPORTANT: The "public" folder should NOT be included in the ".gitignore" file
+gulp.task('deploy', function() {
   runSequence(
     'clean',
     ['sass', 'js', 'image', 'nunjucks', 'vendor', 'custom-files'],
-    'push-gh-master',
-    'push-gh-pages',
-    callback
+    'git-push'
   );
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
   gulp.watch('source/static/**/*.js', ['js']);
   gulp.watch('source/sass/**/*.scss', ['sass']);
   gulp.watch('source/templates/**/*.html', ['nunjucks']);
@@ -96,7 +87,7 @@ gulp.task('watch', function () {
   gulp.watch('source/static/img/**/*', ['images']);
 });
 
-gulp.task('default', function (callback) {
+gulp.task('default', function(callback) {
   runSequence(
     'clean',
     ['sass', 'js', 'image', 'nunjucks', 'vendor'],
